f018e31191ca97ff954ed6bbe9996a3c3797e9c8
Chris Seckler <chrisseckler@gmail.com>
2017-03-19 23:05:08 -0700

    Refactor

--- js/functions.js
+++ js/functions.js
@@ -27,6 +27,135 @@ function preLoadImages(photos) {
   });
 }
 
+function createMap() {
+  var map = new L.Map('map', {
+    layers: [basic],
+    center: new L.LatLng(29.72, -95.35),
+    zoom: 10,
+    maxZoom: 18,
+    zoomControl: false,
+    scrollWheelZoom: false
+  });
+
+  // Add the zoom control manually at a specified position on the map
+  var zoomControl = L.control.zoom({
+      position: 'topright'
+  });
+  map.addControl(zoomControl);
+
+  // Add draw toolbar to the map
+  var drawnItems = new L.FeatureGroup();
+  var drawControl = new L.Control.Draw({
+    position: 'topright',
+    edit: {
+      featureGroup: drawnItems,
+      edit: false,
+      remove: false
+    },
+    draw: {
+      circle: false,
+      marker: false,
+      polyline: false
+    }
+  });
+  map.addLayer(drawnItems);
+  map.addControl(drawControl);
+
+  return map;
+}
+
+function removeShapeAndMarkers(map, shape, markers) {
+  removeMarkers(map, markers);
+  removeShape(map, shape);
+}
+
+function removeMarkers(map, markers) {
+  // Remove markers
+  $.each (markers, function (i) {
+    map.removeLayer(markers[i])
+  });
+}
+
+function removeShape(map, shape) {
+  // Remove shape
+  if (shape != null) {
+    map.removeLayer (shape);
+  }
+}
+
+function getArraySearchFormValues($searchFormElement) {
+  return $searchFormElement.find(":input").filter(function () { return $.trim(this.value).length > 0; }).serializeArray();
+}
+
+function getShapeBounds(shape) {
+  return $.map(shape.getLatLngs()[0], function(point) {
+    return { name: "points", value: point.lat + "," + point.lng };
+  });
+}
+
+function getMapBoundsInViewingArea(map) {
+  var data = [];
+  var mapBounds = {
+    northWest: { lat: map.getBounds().getNorthWest().lat - .05, lng: map.getBounds().getNorthWest().lng + .05 },
+    northEast: { lat: map.getBounds().getNorthEast().lat - .05, lng: map.getBounds().getNorthEast().lng - .25 },
+    southEast: { lat: map.getBounds().getSouthEast().lat + .12, lng: map.getBounds().getSouthEast().lng - .25 },
+    southWest: { lat: map.getBounds().getSouthWest().lat + .12, lng: map.getBounds().getSouthWest().lng + .05 }
+  };
+
+  $.each (mapBounds, function (i, latLngSet) {
+    data.push({
+      name: 'points',
+      value: latLngSet.lat + ',' + latLngSet.lng,
+      data: {
+        lat: latLngSet.lat,
+        lng: latLngSet.lng
+      }
+    });
+  });
+
+  return data;
+}
+
+function showMapViewingArea(map) {
+  // Verify corners of map
+  var mapBounds = getMapBoundsInViewingArea(map);
+
+  $.each (mapBounds, function (i, latLngSet) {
+    var markerLocation = new L.LatLng(latLngSet.data.lat, latLngSet.data.lng);
+    var marker = new L.Marker(markerLocation, {icon: L.spriteIcon('green')});
+    map.addLayer(marker);
+  });
+}
+
+function performSearch(map, $searchResultsGrid, markers, shape, searchParameters, shouldRemoveMarkers) {
+  var data;
+  var searchBounds;
+  var searchResults;
+
+  // 1. Remove the existing markers if necessary
+  if (shouldRemoveMarkers) {
+    removeMarkers(map, markers);
+  }
+
+  // 2. Determine the bounds of the search
+  if (shape != null) {
+    // If the shape has been drawn already, use the shape as the bounds of the search
+    searchBounds = getShapeBounds(shape);
+  } else {
+    // Otherwise use the map as the bounds of the search
+    searchBounds = getMapBoundsInViewingArea(map);
+  }
+
+  // 3. Combine the determined bounds and search parameters (if they are set)
+  data = $.merge( $.merge( [], searchBounds ), searchParameters );
+
+  // 4. Request the search results
+  searchResults = getSearchResults(data);
+
+  // 5. Display the search results on the map and grid
+  displaySearchResults(searchResults, $searchResultsGrid, map, markers);
+}
+
 function createMarkerOnMap(listing, map, markers) {
   // Create a new marker based on the coordinates of the current listing
   var markerLocation = new L.LatLng(listing.geo.lat, listing.geo.lng);
--- js/scripts.js
+++ js/scripts.js
@@ -1,159 +1,77 @@
 $(document).ready(function() {
   var $spinner = $('#spinner');
   var $searchResultsGrid = $('#search-results-grid');
+  var $searchFormElement = $('#properties-search-form');
+  var $searchBox = $searchFormElement.parent();
   var markers = [];
-  var polygon = null;
+  var shape = null;
+  var searchParameters = [];
 
-  var map = new L.Map('map', {
-    layers: [basic],
-    center: new L.LatLng(29.72, -95.35),
-    zoom: 10,
-    maxZoom: 18,
-    zoomControl: false,
-    scrollWheelZoom: false
-  });
+  var map = createMap();
 
-  // Add the zoom control manually at a specified position on the map
-  var zoomControl = L.control.zoom({
-      position: 'topright'
-  });
-  map.addControl(zoomControl);
+  // Debugging: Show the bounds of the map where properties will be shown for search results when the map tool is not used
+  //showMapViewingArea(map);
 
-  // Add draw toolbar to the map
-  var drawnItems = new L.FeatureGroup();
-  var drawControl = new L.Control.Draw({
-    position: 'topright',
-    edit: {
-      featureGroup: drawnItems,
-      edit: false,
-      remove: false
-    },
-    draw: {
-      circle: false,
-      marker: false,
-      polyline: false
-    }
-  });
-  map.addLayer(drawnItems);
-  map.addControl(drawControl);
+  // On Page load, search for all properties on the current map viewing area
+  performSearch(map, $searchResultsGrid, markers, shape, searchParameters, true);
 
   map.on('draw:created', function (e) {
-    // Remove markers and polygon from the last run
-    $.each (markers, function (i) { map.removeLayer(markers[i]) });
-    if (polygon != null) map.removeLayer (polygon);
+    // Remove the existing shape and markers
+    removeShapeAndMarkers(map, shape, markers);
 
-    var latLngs = $.map(e.layer.getLatLngs()[0], function(o) {
-      return { name: "points", value: o.lat + "," + o.lng };
+    // Save the drawn shape for reference
+    shape = e.layer;
+
+    // Perform the search
+    performSearch(map, $searchResultsGrid, markers, shape, searchParameters, false);
+
+    // Add the drawn shape to the map
+    map.addLayer(shape);
+
+    // Add 'Remove Shape' button above the search box
+    $searchBox.prepend('<button id="remove-shape-btn" class="uk-position-top-right uk-button uk-button-primary">Remove Shape</button>');
+  });
+
+  // When the search parameters form is used to filter search results
+  $('#properties-search-form').on('submit', function(e) {
+    e.preventDefault();
+
+    // Only add the filters if any were chosen (remove this when validation is added to the form)
+    //if (searchParameters.length > 0) {
+      // Save the chosen search parameters for reference
+      searchParameters = getArraySearchFormValues($searchFormElement);
+
+      // Perform the search
+      performSearch(map, $searchResultsGrid, markers, shape, searchParameters, true);
+
+      // Add 'Clear Filters' button above the search box
+      $searchBox.prepend('<button id="clear-search-parameters-btn" class="uk-position-top-left uk-button uk-button-primary">Clear Filters</button>');
+    //}
+  });
+
+  // Change disabled attribute on search box submit button depending on if there is a value in any field
+  $('#properties-search-form :input').on('change keyup', function() {
+    $('#search-box-search-btn').prop("disabled", true);
+    $('#properties-search-form :input').each(function() {
+      if( $(this).val().length > 0 ) {
+        $('#search-box-search-btn').prop("disabled", false);
+        return false; // break the loop
+      }
     });
+  });
 
-    //getSearchResults(latLngs, displaySearchResults);
-    var searchResults = getSearchResults(latLngs);
-    displaySearchResults(searchResults, $searchResultsGrid, map, markers);
+  $searchBox.on('click', '#clear-search-parameters-btn', function() {
+    $searchFormElement[0].reset();
+    $('#search-box-search-btn').prop("disabled", true);
+    searchParameters = [];
+    performSearch(map, $searchResultsGrid, markers, shape, searchParameters, true);
+    $(this).remove();
+  });
 
-//     $.ajax({
-//       url: 'https://api.simplyrets.com/properties',
-//       data: latLngs,
-//       beforeSend: function(xhr) {
-//         $spinner.show({
-//           complete: function() {
-//             $(this).css('opacity', '1');
-//           }
-//         });
-//         xhr.setRequestHeader("Authorization", "Basic " + btoa("simplyrets:simplyrets"));
-//       },
-//       success: function(response) {
-//         var spinnerTimeOut = setTimeout(function(){
-//           $spinner
-//             .css('opacity', '0')
-//             .on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd",
-//              function(e){
-//                 $(this).hide();
-//                 $(this).off(e);
-//              });
-//
-//              clearTimeout(spinnerTimeOut);
-//         }, 500);
-//
-//         $searchResultsGrid.html('');
-//
-//         $.each (response, function (i, listing) {
-//           //Pre-load images for the current listing
-//           $.each(listing.photos, function(i, photo){
-//             var img = new Image();
-//             img.src = photo;
-//             //console.log(img);
-//             console.log('finished preloading images');
-//           });
-//
-//           // Create a new marker based on the coordinates of the current listing
-//           var markerLocation = new L.LatLng(listing.geo.lat, listing.geo.lng);
-//           var marker = new L.Marker(markerLocation);
-// console.log('binding popup');
-//           // Define the popup contents to show when the marker is clicked
-//           marker.bindPopup(
-//             _.templateFromUrl('templates/listing-popup-template.html',
-//             $.extend({}, listing, {
-//               listPrice: $.number(listing.listPrice, 0)
-//             }))
-//           );
-//
-//           // When the popup opens after a user clicks it, perform a series of operations
-//           marker.on('popupopen', function (e) {
-//             var popup = e.popup;
-//             var $popup = $(popup.getElement());
-//             var $imagesContent = $popup.find('.images-content');
-//
-//             // Center the map on the marker and popup
-//             var px = map.project(popup.getLatLng()); // find the pixel location on the map where the popup anchor is
-//             px.y -= $popup.height()/2 // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
-//             map.panTo(map.unproject(px),{animate: true}); // pan to new center
-//
-//             // Initialize the flexslider to carousel through the images
-//             $imagesContent.flexslider({
-//               animation: "slide",
-//               customDirectionNav: $imagesContent.next(".custom-navigation").find('a'),
-//               controlNav: false,
-//               easing: "linear"
-//             });
-//
-//             $('#listing-detail').on('beforeshow', function() {
-//               $(this).find('.listing-detail-content').html(
-//                 _.templateFromUrl('templates/listing-detail-content.html',
-//                 $.extend({}, listing, {
-//                   listPrice: $.number(listing.listPrice, 0),
-//                   stateAbbreviation: convertState(listing.address.state, 'abbreviation')
-//                 }))
-//               );
-//             });
-//
-//             $('#listing-detail').on('show', function() {
-//               var $listingDetailImagesContent = $(this).find('.images-content');
-//               $listingDetailImagesContent.flexslider({
-//                 animation: "slide",
-//                 customDirectionNav: $listingDetailImagesContent.next(".custom-navigation").find('a'),
-//                 controlNav: false,
-//                 easing: "linear"
-//               });
-//             });
-//           });
-//
-//           map.addLayer(marker);
-//           markers.push(marker);
-//
-//           // Add the results to the search resuls grid
-//           $searchResultsGrid.append(
-//             _.templateFromUrl('templates/search-result-single.html',
-//             $.extend({}, listing, {
-//               listPrice: $.number(listing.listPrice, 0),
-//               stateAbbreviation: convertState(listing.address.state, 'abbreviation')
-//             }))
-//           );
-//
-//         });
-//       }
-//     });
-
-    polygon = e.layer;
-    map.addLayer(e.layer);
+  $searchBox.on('click', '#remove-shape-btn', function() {
+    removeShapeAndMarkers(map, shape, markers);
+    shape = null;
+    performSearch(map, $searchResultsGrid, markers, shape, searchParameters, false);
+    $(this).remove();
   });
 });
